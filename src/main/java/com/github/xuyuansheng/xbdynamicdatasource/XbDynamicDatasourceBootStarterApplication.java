e0810b0350fb8dd0239bb7f3925326656cf382e8
浮躁的菜 <804288658@qq.com>
2019-12-11 21:17:59 +0800

    first commit

---
+++ src/main/java/com/github/xuyuansheng/xbdynamicdatasource/XbDynamicDatasourceBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.xuyuansheng.xbdynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbDynamicDatasourceBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbDynamicDatasourceBootStarterApplication.class, args);
+    }
+
+}
